a87570acd3f56808c671bcb5493f2b7a51c644b1
Ritik Bokre <ritikbokre100@gmail.com>
2023-10-24 07:50:36 +0000

    close popup outside click

--- index.js
+++ index.js
@@ -4,46 +4,50 @@ const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
 const declineBtn = document.getElementById('decline-btn')
 const modalChoiceBtns = document.getElementById('modal-choice-btns')
+const modalBlocker = document.getElementById('modal-blocker');
 
-setTimeout(function(){
+setTimeout(function () {
     modal.style.display = 'inline'
 }, 1500)
 
-modalCloseBtn.addEventListener('click', function(){
+modalCloseBtn.addEventListener('click', function () {
     modal.style.display = 'none'
-}) 
+})
 
-declineBtn.addEventListener('mouseenter', function(){
+declineBtn.addEventListener('mouseenter', function () {
     modalChoiceBtns.classList.toggle('modal-btns-reverse')
-}) 
+})
 
-consentForm.addEventListener('submit', function(e){
+consentForm.addEventListener('submit', function (e) {
     e.preventDefault()
-    
+
     const consentFormData = new FormData(consentForm)
     const fullName = consentFormData.get('fullName')
-    
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
-    </div>` 
-    
-    setTimeout(function(){
+    </div>`
+
+    setTimeout(function () {
         document.getElementById('upload-text').innerText = `
         Making the sale...`
     }, 1500)
-    
-    
-    setTimeout(function(){
-        document.getElementById('modal-inner').innerHTML = `
+
+
+    setTimeout(function () {
+        document.getElementById('modal-inner-content').innerHTML = `
         <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
         </div>
     `
-    modalCloseBtn.disabled = false
+        modalCloseBtn.disabled = false;
+        modalBlocker.addEventListener('click', function () {
+            modal.style.display = 'none'
+        })
     }, 3000)
-  
+
 }) 
